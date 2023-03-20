5bd896aa865ee6bb1fe3e3cbf637908035425942
Quyet <20020467@vnu.edu.vn>
2023-03-20 16:48:09 +0700

    test screen

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,17 +1,27 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { View, Text, Button, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Go to History Screen"
-        onPress={() => navigation.navigate("History")}
-      />
-    </View>
+    // <View>
+    //   <Text>HomeScreen</Text>
+    //   <Button
+    //     title="Go to History Screen"
+    //     onPress={() => navigation.navigate("History")}
+    //   />
+    // </View>
+    <SafeAreaView>
+      <Text>test</Text>
+    </SafeAreaView>
   );
 };
 
